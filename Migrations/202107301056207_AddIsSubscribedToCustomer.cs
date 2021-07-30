52e0f0bc24cfb45ed6fd2e68e7c9ed8d4d6a4d89
Lindos-Mahadi <lindosmahadi@.com>
2021-07-30 16:58:11 +0600

    Subscribed To NewsLetter in CustomerModel

---
+++ Migrations/202107301056207_AddIsSubscribedToCustomer.cs
@@ -0,0 +1,18 @@
+﻿namespace MVC_FIRST_PROJECT.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddIsSubscribedToCustomer : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "IsSubscribedToNewsLetterCustomer", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Customers", "IsSubscribedToNewsLetterCustomer");
+        }
+    }
+}
--- Models/Customer.cs
+++ Models/Customer.cs
@@ -9,5 +9,6 @@ namespace MVC_FIRST_PROJECT.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsLetterCustomer { get; set; }
     }
 }
