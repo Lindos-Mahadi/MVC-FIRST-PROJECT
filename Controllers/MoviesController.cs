eee7240d61bde2f640119ed82ec73b709afd14e5
Lindos-Mahadi <lindosmahadi@.com>
2021-07-30 10:25:24 +0600

    Movie Controller Created

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using MVC_FIRST_PROJECT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_FIRST_PROJECT.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "lindos" };
+            return View(movie);
+        }
+    }
+}
