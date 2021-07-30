01905718dc7e256765d36164df2f666bb5aa8cc3
Lindos-Mahadi <lindosmahadi@.com>
2021-07-30 09:34:22 +0600

    Inital MVC BOOK Project

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_FIRST_PROJECT.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_FIRST_PROJECT.Startup))]
+namespace MVC_FIRST_PROJECT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
