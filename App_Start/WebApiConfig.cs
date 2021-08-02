edea3afc8a23034976f930127c4486d2be1a4132
Lindos-Mahadi <lindosmahadi@.com>
2021-08-02 14:34:50 +0600

    Using Camel Notation in WebApiConfig

--- App_Start/WebApiConfig.cs
+++ App_Start/WebApiConfig.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Newtonsoft.Json.Serialization;
 using System.Web.Http;
+using Newtonsoft.Json;
 
 namespace MVC_FIRST_PROJECT
 {
@@ -9,6 +8,10 @@ namespace MVC_FIRST_PROJECT
     {
         public static void Register(HttpConfiguration config)
         {
+            var settings = config.Formatters.JsonFormatter.SerializerSettings;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            settings.Formatting = Formatting.Indented;
+
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
