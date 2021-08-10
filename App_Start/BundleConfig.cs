6b3d4957cef89509cfa7acd768e46f722cb9fc9e
Lindos-Mahadi <lindosmahadi@.com>
2021-08-10 20:26:04 +0600

    Autocompletation Using Twitter.Typehead and Instlall

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -20,7 +20,8 @@ namespace MVC_FIRST_PROJECT
                         "~/Scripts/bootbox.js",
                         "~/Scripts/respond.js",
                         "~/Scripts/datatables/jquery.datatables.js",
-                        "~/Scripts/datatables/datatables.bootstrap.js"
+                        "~/Scripts/datatables/datatables.bootstrap.js",
+                        "~/Scripts/typeahead.bundle.js"
                         ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -39,6 +40,7 @@ namespace MVC_FIRST_PROJECT
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/datatables/css/datatables.bootstrap.css",
+                      "~/content/typeahead.css",
                       "~/Content/site.css"
                       ));
         }
